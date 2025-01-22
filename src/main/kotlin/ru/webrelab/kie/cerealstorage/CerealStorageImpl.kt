54db9d8a7e2464551bfa24f1b9c177539dd0c8b9
Evgeniya <jane.mischenko@gmail.com>
2025-01-23 00:13:00 +0300

    add CerealStorageImpl and addCereal Tests

--- src/main/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImpl.kt
+++ src/main/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImpl.kt
@@ -20,4 +20,60 @@ class CerealStorageImpl(
 
     private val storage = mutableMapOf<Cereal, Float>()
 
+    override fun addCereal(cereal: Cereal, amount: Float): Float {
+        require(amount > 0) { "Кол-во не может быть отрицательным" }
+
+        val currentAmount = storage.getOrDefault(cereal, 0f)
+        val availableSpace = getSpace(cereal)
+
+        return if (availableSpace >= amount) {
+            storage[cereal] = currentAmount + amount
+            0f
+        } else {
+            storage[cereal] = currentAmount + availableSpace
+            amount - availableSpace
+        }
+    }
+
+    override fun getCereal(cereal: Cereal, amount: Float): Float {
+        require(amount > 0) { "Кол-во не может быть отрицательным" }
+
+        val currentAmount = storage.getOrDefault(cereal, 0f)
+        return if (currentAmount >= amount) {
+            storage[cereal] = currentAmount - amount
+            amount
+        } else {
+            storage.remove(cereal)
+            currentAmount
+        }
+    }
+
+    override fun removeContainer(cereal: Cereal): Boolean {
+        val currentAmount = storage.getOrDefault(cereal, 0f)
+        return if (currentAmount == 0f) {
+            storage.remove(cereal)
+            true
+        } else {
+            false
+        }
+    }
+
+    override fun getAmount(cereal: Cereal): Float {
+        return storage.getOrDefault(cereal, 0f)
+    }
+
+    override fun getSpace(cereal: Cereal): Float {
+        val currentAmount = storage.getOrDefault(cereal, 0f)
+        return containerCapacity - currentAmount
+    }
+
+    override fun toString(): String {
+        return buildString {
+            append("Хранилище крупы: \n")
+            storage.forEach { (cereal, amount) ->
+                append("- ${cereal.name}: $amount/$containerCapacity\n")
+            }
+        }
+    }
+
 }
--- src/test/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImplTest.kt
+++ src/test/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImplTest.kt
@@ -2,10 +2,18 @@ package ru.webrelab.kie.cerealstorage
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.assertj.core.api.Assertions.assertThat
+
 
 class CerealStorageImplTest {
 
-    private val storage = CerealStorageImpl(10f, 20f)
+    // private val storage = CerealStorageImpl(10f, 20f)
+    private val storageCapacity = 50f
+    private val containerCapacity = 10f
+    private val storage = CerealStorageImpl(containerCapacity, storageCapacity)
+    private val cereal = Cereal.MILLET
+
 
     @Test
     fun `should throw if containerCapacity is negative`() {
@@ -13,4 +21,35 @@ class CerealStorageImplTest {
             CerealStorageImpl(-4f, 10f)
         }
     }
+
+    @Test
+    fun `addCereal should throw IllegalArgumentException if amount is negative`() {
+        assertThrows<IllegalArgumentException> {
+            storage.addCereal(cereal, -5f)
+        }
+    }
+
+    @Test
+    fun `addCereal should throw IllegalArgumentException if storage capacity is exceeded`() {
+        repeat((storageCapacity / containerCapacity).toInt()) {
+            storage.addCereal(Cereal.MILLET, containerCapacity)
+        }
+        assertThrows<IllegalArgumentException> {
+            storage.addCereal(cereal, 5f)
+        }
+    }
+
+    @Test
+    fun `addCereal should add cereal if container is exist`() {
+        storage.addCereal(cereal, 5f)
+        assertThat(storage.getAmount(cereal)).isEqualTo(5f)
+    }
+
+    @Test
+    fun `addCereal should return remainder if container overflows`() {
+        val remainder = storage.addCereal(cereal, 15f)
+        assertThat(remainder).isEqualTo(5f)
+        assertThat(storage.getAmount(cereal)).isEqualTo(containerCapacity)
+    }
+
 }
